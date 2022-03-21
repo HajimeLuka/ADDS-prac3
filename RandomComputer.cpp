988c20d79988a70db3ec81a38ffef6238d4164eb
ADDS-prac1.git <a1826411@student.adelaide.edu.au>
2022-03-21 11:04:22 +0000

    please

---
+++ RandomComputer.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+#include "RandomComputer.h"
+#include "Computer.h"
+#include "Player.h"
+
+RandomComputer::RandomComputer():Computer(){
+    int random=(rand()%2)+1;
+
+    if (random==1){
+        move='R';
+    }
+    if (random==2){
+        move=='P';
+    if (random==3){
+        move=='S';
+    }
+
+}
+
